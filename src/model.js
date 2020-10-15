c224c309a1ce57012d45d9ae2c297baff37cf251
Anton_Poludnitsin <poludnitsin.ap@pg.com>
2020-10-15 15:58:43 +0400

    add common class Block

--- src/model.js
+++ src/model.js
@@ -1,4 +1,5 @@
 import image from './assets/image.png'
+import { Block } from "./classes/blocks";
 
 const text = `
 Крутые видео и уроки по JavaScript тут: 
@@ -8,62 +9,48 @@ const text = `
  Присоединяйся!
 `
 
-export const model = [
-  {
-    type: "title",
-    value: "Конструктор сайтов на чистом JS",
-    options: {
-      tag: "h2",
-      styles: {
-        background: `linear-gradient(to right, #ff0099, #493240)`,
-        color: '#fff',
-        padding: '1.5rem',
-        'text-align': 'center'
+const columnValue = [
+  'Приложение на чистом JavaScript, без использования библиотек',
+  'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс',
+  'JavaScript - это просто, интересно. Научись создавать любые UI своими руками',
+]
 
-      }
+export const model = [
+  new Block('title', 'Конструктор сайтов на чистом JS', {
+    tag: "h2",
+    styles: {
+      background: `linear-gradient(to right, #ff0099, #493240)`,
+      color: '#fff',
+      padding: '1.5rem',
+      'text-align': 'center'
     }
-  },
-  {
-    type: "text",
-    value: text,
-    options: {
-      styles: {
-        background: 'linear-gradient(to left, #f2994a, #f2c94c)',
-        padding: '1rem',
-        'font-weight': 'bold'
-      }
+  })
+  ,
+  new Block('text', text, {
+    styles: {
+      background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+      padding: '1rem',
+      'font-weight': 'bold'
     }
-  },
-  {
-    type: "columns",
-    value: [
-      'Приложение на чистом JavaScript, без использования библиотек',
-      'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс',
-      'JavaScript - это просто, интересно. Научись создавать любые UI своими руками',
-    ],
-    options: {
-      styles: {
-        background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
-        padding: '2rem',
-        color: '#fff',
-        'font-weight': 'bold'
-      }
+  }),
+  new Block('columns', columnValue, {
+    styles: {
+      background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+      padding: '2rem',
+      color: '#fff',
+      'font-weight': 'bold'
     }
-  },
-  {
-    type: 'image',
-    value: image,
-    options: {
-      styles: {
-        padding: '2rem 0',
-        display: 'flex',
-        'justify-content': 'center'
-      },
-      imageStyles: {
-        width: '500px',
-        height: 'auto'
-      },
-      alt: "Это картинка"
-    }
-  }
+  }),
+  new Block('image', image, {
+    styles: {
+      padding: '2rem 0',
+      display: 'flex',
+      'justify-content': 'center'
+    },
+    imageStyles: {
+      width: '500px',
+      height: 'auto'
+    },
+    alt: "Это картинка"
+  }),
 ]
